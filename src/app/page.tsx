a39381646a0d3d6a78942ec914fc410214ea62b3
loukalost <lk.lemonnier@gmail.com>
2025-11-01 12:49:03 +0100

    feat: Update app metadata and add icons for better branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 // Clean Architecture: Presentation layer imports application components
-import BenefitsSection from '@/components/sections/benefits-section'
 import ActionsSection from '@/components/sections/actions-section'
-import Header from '@/components/sections/header'
+import BenefitsSection from '@/components/sections/benefits-section'
 import Footer from '@/components/sections/footer'
+import Header from '@/components/sections/header'
 import HeroSection from '@/components/sections/hero-section'
 import MonstersSection from '@/components/sections/monsters-section'
 import NewsletterSection from '@/components/sections/newsletter-section'
+import { Metadata } from 'next'
+
+export const metadata: Readonly<Metadata> = {
+  title: 'Tamagotcho - Adopte et prends soin de ton compagnon virtuel',
+  description: 'Tamagotcho est une application web où tu peux adopter, nourrir, jouer et faire évoluer ton propre monstre virtuel. Rejoins-nous pour une aventure amusante et interactive !',
+  keywords: 'Tamagotcho, monstre virtuel, adoption, jeu, aventure',
+  openGraph: {
+    title: 'Tamagotcho - Adopte et prends soin de ton compagnon virtuel',
+    description: 'Tamagotcho est une application web où tu peux adopter, nourrir, jouer et faire évoluer ton propre monstre virtuel. Rejoins-nous pour une aventure amusante et interactive !'
+  },
+  twitter: {
+    title: 'Tamagotcho - Adopte et prends soin de ton compagnon virtuel',
+    description: 'Tamagotcho est une application web où tu peux adopter, nourrir, jouer et faire évoluer ton propre monstre virtuel. Rejoins-nous pour une aventure amusante et interactive !'
+  }
+}
 
 // Single Responsibility: Home page orchestrates the layout of sections
 export default function Home (): React.ReactNode {
