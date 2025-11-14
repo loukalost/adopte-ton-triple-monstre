d26809b0f665683eda3a233d287bd1e049f904c8
loukalost <lk.lemonnier@gmail.com>
2025-11-14 12:02:55 +0100

    refactor: update AppLayout to use wrapper components for header and navigation

--- src/components/navigation/app-layout.tsx
+++ src/components/navigation/app-layout.tsx
@@ -1,7 +1,5 @@
-'use client'
-
-import AppHeader from './app-header'
-import BottomNav from './bottom-nav'
+import AppHeaderWrapper from './app-header-wrapper'
+import BottomNavWrapper from './bottom-nav-wrapper'
 
 interface AppLayoutProps {
   children: React.ReactNode
@@ -11,12 +9,14 @@ interface AppLayoutProps {
  * Layout principal de l'application avec navigation
  *
  * Ce composant wrap les pages authentifiées et ajoute :
- * - AppHeader en haut sur desktop (md et plus)
- * - BottomNav en bas sur mobile/tablette (moins de md)
+ * - AppHeader en haut sur desktop (md et plus) avec wallet balance
+ * - BottomNav en bas sur mobile/tablette (moins de md) avec wallet balance
  * - Padding bottom sur mobile pour éviter que le contenu soit caché par la barre
  *
  * Responsabilité unique : Orchestrer l'affichage de la navigation
  * en fonction de la taille de l'écran
+ *
+ * Note : Les wrappers gèrent la récupération du wallet côté serveur
  *
  * @param {AppLayoutProps} props - Les propriétés du composant
  * @param {React.ReactNode} props.children - Le contenu de la page
@@ -24,16 +24,16 @@ interface AppLayoutProps {
 export default function AppLayout ({ children }: AppLayoutProps): React.ReactNode {
   return (
     <div className='min-h-screen flex flex-col'>
-      {/* Header desktop (masqué sur mobile) */}
-      <AppHeader walletBalance={0} />
+      {/* Header desktop (masqué sur mobile) - récupère le wallet */}
+      <AppHeaderWrapper />
 
       {/* Contenu principal avec padding pour la barre mobile */}
       <main className='flex-1 pb-20 md:pb-0'>
         {children}
       </main>
 
-      {/* Navigation mobile (masquée sur desktop) */}
-      <BottomNav walletBalance={0} />
+      {/* Navigation mobile (masquée sur desktop) - récupère le wallet */}
+      <BottomNavWrapper />
     </div>
   )
 }
